417203c4554b4fe3522c65b25a46876f4099ae1b
Sam Smoot <ssmoot@gmail.com>
2012-11-29 15:37:57 -0600

    Adding IDEA project modules.

---
+++ ext/java/Watcher.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+
+public class Watcher {
+
+    /**
+     * Main program.
+     * * @param args Command line arguments - dirs to watch.
+     * * @throws IOException in case of I/O problems.
+     * * @throws InterruptedException in case the thread was interrupted during watchService.take().
+     */
+    public static void main(final String... args) throws IOException, InterruptedException {
+        final FileSystem fileSystem = FileSystems.getDefault();
+        try (final WatchService watchService = fileSystem.newWatchService()) {
+            final Map<WatchKey, Path> keyMap = new HashMap<WatchKey, Path>();
+            for (final String arg : args.length > 0 ? args : new String[]{"."}) {
+                final Path path = Paths.get(arg);
+                keyMap.put(path.register(watchService, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY), path);
+            }
+            WatchKey watchKey;
+            do {
+                watchKey = watchService.take();
+                final Path eventDir = keyMap.get(watchKey);
+                for (final WatchEvent<?> event : watchKey.pollEvents()) {
+                    final WatchEvent.Kind kind = event.kind();
+                    final Path eventPath = (Path) event.context();
+                    System.out.println(eventDir + ": " + event.kind() + ": " + event.context());
+                }
+            } while (watchKey.reset());
+        }
+    }
+}
