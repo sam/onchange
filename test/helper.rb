3e3bc83b4d06bc360e30887d13a24d48c35f1d38
Sam Smoot <ssmoot@gmail.com>
2012-11-29 14:55:04 -0600

    Added Doubleshot file, a place-holder for Watcher, and a sample test folder.

---
+++ test/helper.rb
@@ -0,0 +1,5 @@
+require "doubleshot/setup"
+
+require "minitest/autorun"
+require "minitest/pride"
+require "minitest/wscolor"
---
+++ test/public_file_handler_spec.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env jruby
+
+# encoding: utf-8
+
+require_relative "helper"
+
+describe org.sam.cotton.PublicFileHandler do
+  it "is a place-holder" do
+    skip
+  end
+end
